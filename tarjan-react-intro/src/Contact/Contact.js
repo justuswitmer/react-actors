c277de7aa0432efb38ab4559f3fe4bc8ef844182
Justus Witmer <justus.witmer@gmail.com>
2020-09-15 21:11:03 -0500

    added in user fields and onChange text

--- tarjan-react-intro/src/Contact/Contact.js
+++ tarjan-react-intro/src/Contact/Contact.js
@@ -2,23 +2,48 @@ import React, { Component } from 'react';
 
 class Contact extends Component {
 
+    state = {
+        firstName: '',
+        lastName: '',
+        bestImpression: ''
+    }
+
     joinUs = () => {
         console.log('in joinUs');
-        // this.setState({
-        //     feeling: 'Clickalus',
-        //     whatDayIsIt: 'Cage Day'
-        //   });
-        // }
-
+        alert('Thanks for joining!')
     } // end joinUs
+
+    firstName = (event) => {
+        console.log('event.target', event.target);
+        this.setState({
+            firstName: event.target.value
+        });
+    }
+
+    lastName = (event) => {
+        console.log('event.target', event.target);
+        this.setState({
+            lastName: event.target.value
+        });
+    }
+
+    bestImpression = (event) => {
+        console.log('event.target', event.target);
+        this.setState({
+            bestImpression: event.target.value
+        });
+    }
+
+
     render() {
         return (
             <div>
                 <h2>Contact</h2>
-                {/* <input type="text" placeholder="First Name" onChange={this.firstName} />
+                <input type="text" placeholder="First Name" onChange={this.firstName} />
                 <input type="text" placeholder="Last Name" onChange={this.lastName} />
-                <input type="text" placeholder="Best Impression" onChange={this.bestImpression} /> */}
+                <input type="text" placeholder="Best Impression" onChange={this.bestImpression} />
                 <button onClick={this.joinUs}>Join Us!</button>
+                <p>{this.state.firstName} {this.state.lastName}'s Best Impression is {this.state.bestImpression}</p>
                 <p>1-800-wemakeskits</p>
                 <p>reactingActors87@react.com</p>
             </div>
