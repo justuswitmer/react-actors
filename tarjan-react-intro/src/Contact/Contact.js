0f6bed4fcbc290b813fbeaafb2037ee5810833bb
Justus Witmer <justus.witmer@gmail.com>
2020-09-15 21:01:46 -0500

    join us button is on the the DOM

--- tarjan-react-intro/src/Contact/Contact.js
+++ tarjan-react-intro/src/Contact/Contact.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
+
+    joinUs = () => {
+        console.log('in joinUs');
+        // this.setState({
+        //     feeling: 'Clickalus',
+        //     whatDayIsIt: 'Cage Day'
+        //   });
+        // }
+
+    } // end joinUs
     render() {
         return (
             <div>
                 <h2>Contact</h2>
+                {/* <input type="text" placeholder="First Name" onChange={this.firstName} />
+                <input type="text" placeholder="Last Name" onChange={this.lastName} />
+                <input type="text" placeholder="Best Impression" onChange={this.bestImpression} /> */}
+                <button onClick={this.joinUs}>Join Us!</button>
                 <p>1-800-wemakeskits</p>
                 <p>reactingActors87@react.com</p>
             </div>
